3c13b1d7ccf57c41f9d1d4d3d61e87f8ead85d10
SingWong <robeenwang@163.com>
2020-04-19 15:05:02 +0800

    day two

---
+++ PythonCrashCourse/Ch5/ex5_1-2testcondition.py
@@ -0,0 +1,19 @@
+'''
+ # -*- coding: utf-8 -*-
+ @Time    : 4/19/2020 1:44 PM
+ @Author  : 思侬
+ @Email   : robeenwang@163.com
+ @File    : ex5_1-2testconditions.py
+ @Software: PyCharm
+'''
+
+car = 'BWM'
+print('If car == bwm?i predict no')
+print(car == 'bwm')
+
+number = 11
+print('is the number == 11?i predict yes')
+print(number == 11)
+
+name = 'BENjiminFrankLin'
+print(name.lower() == 'benjiminfranklin')
