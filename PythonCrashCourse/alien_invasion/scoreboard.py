b6395fee77acccbc5b26b6a8201c1dd81d2e8df3
SingWong <robeenwang@163.com>
2020-04-23 16:44:57 +0800

    alien invasion

--- PythonCrashCourse/alien_invasion/scoreboard.py
+++ PythonCrashCourse/alien_invasion/scoreboard.py
@@ -22,7 +22,7 @@ class ScoreBoard():
 
         #显示得分时的字体设置
         self.text_color = (30,30,30)
-        self.font = pygame.font.SysFont(None,48)
+        self.font = pygame.font.SysFont('arial',48)
         #准备初始得分图像
         self.prep_score()
         self.prep_high_score()
