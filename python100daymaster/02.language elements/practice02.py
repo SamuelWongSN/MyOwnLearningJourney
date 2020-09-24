dd21831e9c6754e3becc2bea235262e59e877017
王思侬 <samuel1997wang@gmail.com>
2020-09-24 20:57:19 +0800

    day2

---
+++ python100daymaster/02.language elements/practice02.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 _*-
+""" 
+@author: WangSN
+@license: Apache Licence 
+@file: practice02.py 
+@time: 2020/09/24
+@contact: samuel1997wang@gmail.com
+@site:  
+@software: PyCharm 
+"""
+r = float(input('Please input the radius:'))
+perimeter = 2 * r * 3.14
+area = 3.14 * r * r
+print('perimeter is %.2f, area is %.2f' % (perimeter, area))
--- python100daymaster/02.language elements/practice03.py
+++ python100daymaster/02.language elements/practice03.py
@@ -2,12 +2,13 @@
 """ 
 @author: WangSN
 @license: Apache Licence 
-@file: practice01.py 
+@file: practice03.py 
 @time: 2020/09/24
 @contact: samuel1997wang@gmail.com
 @site:  
 @software: PyCharm 
 """
-F = float(input('Please input F temper:'))
-C = (F - 32) / 1.8
-print('temper in C is %.2f' % C)
+year = int(input('Please input the year:'))
+is_leap = year % 4 == 0 and year % 100 != 0\
+          or year % 400 == 0
+print(is_leap)
